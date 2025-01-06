94b88a0cfc34405bc4e7843c6a400232098d9ad2
DogFortune <dogfortune.2000@gmail.com>
2025-01-06 22:25:18 +0900

    localstackのテスト

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,50 @@
+import pytest
+import boto3
+import os
 from create_param import main
+
+
+AWS_PROFILE = os.getenv("AWS_PROFILE", "localstack")
+AWS_REGION = os.getenv("AWS_REGION", "ap-northeast-1")
+ENDPOINT = os.getenv("LOCALSTACK_ENDPOINT", "http://localhost:4566")
+
+
+@pytest.fixture(scope="session")
+def s3_local():
+    boto3.setup_default_session(profile_name=AWS_PROFILE)
+    s3 = boto3.client("s3", region_name=AWS_REGION, endpoint_url=ENDPOINT)
+    return s3
+
+
+@pytest.fixture()
+def setup_s3(s3_local):
+    """
+    S3へのテストデータ作成
+
+    :param s3_local: _description_
+    :type s3_local: _type_
+    """
+    bucket_name = "sample"
+    object_key = "dummy.txt"
+
+    s3_local.create_bucket(
+        Bucket=bucket_name,
+        CreateBucketConfiguration={"LocationConstraint": "ap-northeast-1"},
+    )
+    s3_local.put_object(Bucket=bucket_name, Key=object_key)
+
+    yield
+
+    s3_local.delete_object(Bucket=bucket_name, Key=object_key)
+    s3_local.delete_bucket(Bucket=bucket_name)
+
+
+def test_local_stack(setup_s3, s3_local):
+    s3_local.head_object(
+        Bucket="sample",
+        Key="dummy.txt",
+    )
+
 
 def test_main():
     actual = main.main(3, 4)
